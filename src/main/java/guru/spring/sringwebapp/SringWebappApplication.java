b133731c5d20c73229c65a1c30c8c4b90d471ecc
essaghir.e@gmail.com <essaghir.e@gmail.com>
2017-12-02 21:47:07 +0000

    init

---
+++ src/main/java/guru/spring/sringwebapp/SringWebappApplication.java
@@ -0,0 +1,12 @@
+package guru.spring.sringwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringWebappApplication.class, args);
+	}
+}
---
+++ src/main/java/guru/spring/sringwebapp/model/Author.java
@@ -0,0 +1,90 @@
+package guru.spring.sringwebapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by said on 29/11/2017.
+ */
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String firstName;
+    String lastname;
+
+    @ManyToMany(mappedBy = "authors")
+    Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+    public Author(String firstName, String lastname, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.books = books;
+    }
+
+    public Author(String firstName, String lastname) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id.equals(author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
